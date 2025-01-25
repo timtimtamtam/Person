2a08a3b4a62ebe76f7d046e51dd68612e940479d
Рыбин Артем Олегович <a.o.rybin@novotelecom.ru>
2025-01-26 01:01:55 +0600

    Fix.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,20 +12,50 @@ public class Main {
         ArrayList<Person> presonList = new ArrayList<>();
         Person person = new Person();
         Scanner sc = new Scanner(System.in);
+        final int INVALID_VALUE_AGE = -1;
+        int yearOfBirth;
 
         System.out.println("Для добавления пользователя введите в правильном порядке: имя, фамилию, отчество." +
                 " Через пробел");
-
         String[] personForSplit = sc.nextLine().split(" ");
+
+        if (personForSplit.length < 3) {
+            System.out.println("Ошибка добавления. Один из параметров не был задан.");
+            return;
+        }
+
         String name = personForSplit[0];
         String familyName = personForSplit[1];
         String middleName = personForSplit[2];
 
-        System.out.println("Проверьте введенные данные: "
+        System.out.println("Введите возраст в числовом формате");
+        int age = INVALID_VALUE_AGE; // пришлось ввести константу чтобы понимать удалось ли вычислить возраст.
+
+        String ageString = sc.nextLine(); // Решил получать возраст через строку т.к. намучался
+                                          // с некорректным поведением сканера после считывания nextInt
+
+        if (!ageString.isEmpty() && ageString.split(" ").length == 1) {
+            try {
+                age = Integer.parseInt(ageString);
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка присвоения возраста пользователю" + e);
+            }
+        } else {
+            System.out.println("Ошибка присвоения возраста пользователю");
+        }
+
+        boolean ageExist = age != INVALID_VALUE_AGE;
+
+        System.out.print("Проверьте введенные данные: "
                 + name + " "
                 + familyName + " "
-                + middleName);
-        System.out.println("Если данные верны, введите - \"Да\" для добавления пользователя, " +
+                + middleName + " ");
+
+        if (ageExist) {
+            System.out.print(age);
+        }
+
+        System.out.println("\nЕсли данные верны, введите - \"Да\" для добавления пользователя, " +
                 "в противном случае введите - \"Нет\". Комманды вводятся без ковычек.");
 
         String answer = sc.nextLine();
@@ -32,13 +64,19 @@ public class Main {
             person.setName(name);
             person.setFamilyName(familyName);
             person.setMiddleName(middleName);
+            if (ageExist) {
+                person.setAge(age);
+                System.out.println(person.getYearOfBirth(age));
+            }
             presonList.add(person);
 
-            System.out.println("Успешно добавлен " + person);
+            if (ageExist) {
+                System.out.println("Успешно добавлен " + person);
+            } else {
+                System.out.println("Успешно добавлен " + person.toStringWhitoutAge());
+            }
         } else {
-            System.out.println("Проргамма завершена");
+            System.out.println("Программа завершена");
         }
-
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,23 @@
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
 public class Person {
 
     private String familyName;
     private String middleName;
     private String name;
+    private int age;
 
-    public void person(String familyName, String middleName, String name) {
+    public void person(String familyName, String middleName, String name, int age) {
         this.familyName = familyName;
         this.middleName = middleName;
         this.name = name;
+        this.age = age;
     }
 
-    public void person(){}
+
+    public void person() {
+    }
 
     public String getFamilyName() {
         return familyName;
@@ -20,27 +27,74 @@ public class Person {
         return middleName;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getYearOfBirth(int age) {
+        if ((age > 0) && (age < 110)) {
+            LocalDate now = LocalDate.now();
+            int year = now.minusYears(age).getYear();
+            String yearOfBirth = String.valueOf(year);
+            return "Год рождения: " + yearOfBirth;
+        } else {
+            return "Ошибка получения года рождения";
+        }
+    }
+
     public String getName() {
         return name;
     }
 
+    public void setAge(int age) {
+        if ((age > 0) && (age < 110)) {
+            this.age = age;
+            System.out.println("Успешно установлено значение поля возраст");
+        } else {
+            System.out.println("Поле возраст не было изменено, введено недопустимое значение.");
+        }
+    }
+
     public void setFamilyName(String familyName) {
-        this.familyName = familyName;
+        if (!familyName.isEmpty()) {
+            this.familyName = familyName;
+            System.out.println("Успешно установлено значение поля фамилия");
+        } else {
+            System.out.println("Поле фамилия не было изменено");
+        }
     }
 
     public void setMiddleName(String middleName) {
-        this.middleName = middleName;
+        if (!middleName.isEmpty()) {
+            this.middleName = middleName;
+            System.out.println("Успешно установлено значение поля отчество");
+        } else {
+            System.out.println("Поле отчество не было изменено");
+        }
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (!name.isEmpty()) {
+            this.name = name;
+            System.out.println("Успешно установлено значение поля имя");
+        } else {
+            System.out.println("Поле имя не было изменено");
+        }
+    }
+
+    public String toStringWhitoutAge() {
+        return "Пользователь: " +
+                "Фамилия " + familyName +
+                ", Имя " + name +
+                ", Отчество " + middleName;
     }
 
     @Override
     public String toString() {
-        return "Пользователь: " + // Решил заменить Человек на Пользовтель, как-то уместнее звучит :)
+        return "Пользователь: " +
                 "Фамилия " + familyName +
                 ", Имя " + name +
-                ", Отчество " + middleName;
+                ", Отчество " + middleName +
+                ", Возраст " + age;
     }
 }
